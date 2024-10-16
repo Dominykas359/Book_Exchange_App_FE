bfb45233f4e18b05627f1cddfae01355e7f5588d
Dominykas Stunžėnas <dominykas.stunzenas@stud.vilniustech.lt>
2024-10-16 10:57:19 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-sm font-bold text-red-500">
+        Hello, Vite + React + TypeScript + Tailwind!
+      </h1>
+    </div>
+  )
+}
+
+export default App
