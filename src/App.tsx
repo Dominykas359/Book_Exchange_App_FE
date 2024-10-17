f040b04e669d9947b8fa205b288031f6cf4dfddf
Dominykas Stunžėnas <dominykas.stunzenas@stud.vilniustech.lt>
2024-10-17 14:00:08 +0300

    creating components, router and eviromental variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { AppRoutes } from './utilities/Routes';
+import Notices from './components/main/Notices';
+import NewNotice from './components/main/NewNotice';
+import Notice from './components/main/Notice';
+import History from './components/main/History';
+import Settings from './components/main/Settings';
+import Login from './components/authentication/Login';
+import Registration from './components/authentication/Registration';
 
 function App() {
 
+  const url = import.meta.env.VITE_BACKEND_URL;
+
+  console.log(url);
+
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-red-500">
-        Hello, Vite + React + TypeScript + Tailwind!
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate to={AppRoutes.NOTICES} />} />
+        <Route path={AppRoutes.NOTICES} element={<Notices />} />
+        <Route path={AppRoutes.NEW_NOTICE} element={<NewNotice />} />
+        <Route path={AppRoutes.NOTICE} element={<Notice />} />
+        <Route path={AppRoutes.HISTORY} element={<History />} />
+        <Route path={AppRoutes.SETTINGS} element={<Settings />} />
+        <Route path={AppRoutes.LOG_IN} element={<Login />} />
+        <Route path={AppRoutes.SIGN_UP} element={<Registration />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/main/NewNotice.tsx
@@ -0,0 +1,11 @@
+
+function NewNotice(){
+
+    return(
+        <div>
+            <h1>New notice</h1>
+        </div>
+    );
+}
+
+export default NewNotice;
