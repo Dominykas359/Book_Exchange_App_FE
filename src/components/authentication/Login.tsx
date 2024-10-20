37386b5452b0f14a869924be08d13fb68702efc3
Dominykas Stunžėnas <dominykas.stunzenas@stud.vilniustech.lt>
2024-10-20 15:17:48 +0300

    functional login and registration without error messages

--- src/components/authentication/Login.tsx
+++ src/components/authentication/Login.tsx
@@ -1,9 +1,68 @@
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AppRoutes } from "../../utilities/Routes";
+
+interface Credentials {
+    email: string,
+    password: string
+}
 
 function Login(){
 
+    const navigate = useNavigate();
+
+    const [credentials, setCredentials] = useState<Credentials>({
+        email: '',
+        password: ''
+    });
+    const [error, setError] = useState<string | null>(null);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setCredentials(prevCredentials => ({
+            ...prevCredentials,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+
+        try {
+            const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/login`, credentials);
+
+            if (response.status === 200) {
+                const data = response.data;
+                localStorage.setItem('user', JSON.stringify(data));
+                navigate(AppRoutes.NOTICES);
+            } else {
+                setError('Wrong email or password. Please try again.');
+            }
+        } catch (error) {
+            setError('Wrong email or password. Please try again.');
+        }
+    }
+
     return(
-        <div>
-            <h1>Login</h1>
+        <div className="min-w-full min-h-full flex justify-center items-center">
+            <div>
+                <h1>Welcome to booky</h1>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="email">Email</label>
+                    <br></br>
+                    <input type="email" name="email" value={credentials.email} onChange={handleInputChange} className="border solid"></input>
+                    <br></br>
+                    <label htmlFor="password">Password</label>
+                    <br></br>
+                    <input type="password" name="password" value={credentials.password} onChange={handleInputChange} className="border solid"></input>
+                    <div className="child">
+                        <button className="authentication-button" type="submit">Login</button>
+                    </div>
+                </form>
+                <span>Don't have an accout yet?</span>
+                <Link to={AppRoutes.SIGN_UP} className="no-underline text-[hsl(272,76%,52%)]">Sign up</Link>
+            </div>
         </div>
     );
 }
