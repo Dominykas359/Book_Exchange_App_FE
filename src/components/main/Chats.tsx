18e1064e64432a79de6b4c9ea0232f479098c593
Dominykas Stunžėnas <dominykas.stunzenas@stud.vilniustech.lt>
2024-10-20 15:22:32 +0300

    new components

---
+++ src/components/main/Chats.tsx
@@ -0,0 +1,14 @@
+import Header from "../../utilities/Header";
+
+function Chats(){
+
+    return(
+        <div>
+            <Header />
+            <h1>Chats</h1>
+        </div>
+        
+    );
+}
+
+export default Chats;
